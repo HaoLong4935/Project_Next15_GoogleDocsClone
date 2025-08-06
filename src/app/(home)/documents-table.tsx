05a66aeb2bfe1699ccfc04d003d66042c2b5815c
HaoLong4935 <longsachse@gmail.com>
2025-08-06 21:03:12 +0700

    Add function: Document Table

--- src/app/(home)/documents-table.tsx
+++ src/app/(home)/documents-table.tsx
@@ -1,7 +1,8 @@
 import { PaginationStatus } from "convex/react"
 import { Doc } from "../../../convex/_generated/dataModel"
 import { LoaderIcon } from "lucide-react"
-
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table"
+import { DocumentRow } from "./document-row"
 interface DocumentsTableProps {
     documents: Doc<"documents">[] | undefined
     loadMore: (numItems: number) => void
@@ -16,9 +17,33 @@ export const DocumentsTable = ({ documents, loadMore, status }: DocumentsTablePr
                     <LoaderIcon className="animate-spin text-muted-foreground" />
                 </div>
             ) : (
-                <div>
-                    Loaded
-                </div>
+                <Table className="mt-4">
+                    <TableHeader>
+                        <TableRow className="hover:bg-transparent border-none">
+                            <TableHead>Name</TableHead>
+                            <TableHead>&nbsp;</TableHead>
+                            <TableHead className="hidden md:table-cell">Shared</TableHead>
+                            <TableHead className="hidden md:table-cell">Created at</TableHead>
+                        </TableRow>
+                    </TableHeader>
+                    {documents.length === 0 ? (
+                        <TableBody>
+                            <TableRow className="hover:bg-transparent border-none">
+                                <TableCell colSpan={4} className="h-24 text-center text-muted-foreground">
+                                    No documents found
+                                </TableCell>
+                            </TableRow>
+                        </TableBody>
+                    )
+                        :
+                        (
+                            <TableBody>
+                                {documents.map((document) => (
+                                    <DocumentRow key={document._id} document={document} />
+                                ))}
+                            </TableBody>
+                        )}
+                </Table>
             )}
         </div>
     )
